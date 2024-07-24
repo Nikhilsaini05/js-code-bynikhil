56e985441eda3bb247d94a0566be479056006470
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2024-07-24 11:23:31 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 112233
+let accountName = "Nikhil"
+var accountNumber = "12345"
+accountBank = "SBI" 
+
+// accountId = 124 not allowed
+accountName = "Rajat"
+accountNumber = "78954"
+accountBank = "ICICI"
+
+console.log(accountId);
+
+console.table([accountId, accountName, accountNumber, accountBank]);
