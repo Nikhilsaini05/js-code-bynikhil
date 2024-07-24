42de5796e2841d5100ea1decaa72a9732b74073e
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2024-07-24 11:23:17 +0000

    dataTypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as js newer version
+//alert("2+2") //we are using node js, Not browser
+
+/* Data Types */            // Premetive Data Types
+
+let name = "nikhil";        // string 
+let age = 22;               // numeric
+let isLoggedIn = true;      // boolean 
+let state;                  // undefined
+                            // symbol => unique identifie
+                            // null => stand-alone value
+
+
