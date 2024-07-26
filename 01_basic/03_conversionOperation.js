25c6d362d57e4d4520d1852df18483fb44e4d048
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2024-07-26 06:41:54 +0000

    conversionoperation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -20,3 +20,41 @@ console.log(booleanIsLoggedIn);
 // 1 => true; 0 => false;
 // "" => false;
 // "nikhil" => true;
+
+let somenumber = "33";
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
+// ******************** Operation *******************************
+
+let var1 = "Nikhil";
+let var2 = " Saini";
+let var3 = var1 + var2;
+console.log(var3);
+
+// console.log(2 + 2);      // Addition operation
+// console.log(2 - 2);      // subtration operation
+// console.log(2 * 2);      // multipication operation
+// console.log(2 ** 3);     // Power operation
+// console.log(2 % 2);      // modules operation
+
+let num1 = num2 = num3 = 2 + 3;     // Not a good way to write a code 
+console.table([num1, num2, num3]);
+
+console.log(2 + "2");
+console.log("2" + 2);
+console.log("2" + 2 + 3);       // If there is any string come first it will add the next string
+                                // with first one &...
+console.log(2 + 2 + "3");       // If and operation come first,So first it perform operation and 
+                                // then add the string with it.
+
+let gameScore = 99;
+// gameScore++;
+++gameScore;
+console.log(gameScore);
+
+
+
+
+
