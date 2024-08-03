3e263ff52437bb9ba0a0fb1c545a2faa447d4611
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2024-08-03 13:51:37 +0000

    array2

---
+++ 02_basic/02_array.js
@@ -0,0 +1,16 @@
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
+// Addition of the array
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+const arr3 = [7,8,9];
+
+const allArr = [...arr1,...arr2,...arr3,10];
+
+console.log(allArr);
+
+
+
