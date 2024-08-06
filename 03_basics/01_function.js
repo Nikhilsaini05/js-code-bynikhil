536c10bc1530163c71f91abacd5006630be82169
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2024-08-06 16:34:53 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,11 @@
+// function 
+function myName() {
+    console.log("N");
+    console.log("I");
+    console.log("k");
+    console.log("H");
+    console.log("I");
+    console.log("l");
+}
+// myName()                 //function call
+
