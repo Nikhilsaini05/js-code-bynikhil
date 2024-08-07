5c92016ccc4f87d9f3f2458f31f7e4a510ad53dc
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2024-08-07 16:54:27 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,11 +1,22 @@
 // function 
 function myName() {
-    console.log("N");
-    console.log("I");
-    console.log("k");
-    console.log("H");
-    console.log("I");
-    console.log("l");
+    console.log("NIKHIL");
+    // console.log("I");
+    // console.log("k");
+    // console.log("H");
+    // console.log("I");
+    // console.log("l");
 }
-// myName()                 //function call
+myName()                 //function call
 
+// example with paremeter
+
+function cookmaggie(maggie,water,masala) {
+    console.log("your maggie will be ready in "
+        + maggie * 2 + " minutes and ingredients are used:- "
+        +maggie + " maggie "
+        + water + " cup of water and "
+        + masala + " packet of masala."
+    )
+}
+cookmaggie(4,8,4);
