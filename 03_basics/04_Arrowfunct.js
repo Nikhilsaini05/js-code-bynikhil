cde856707e2dac83022f2a66437255861ba56c9b
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2024-08-31 10:45:26 +0000

    arrowfunction

--- 03_basics/04_Arrowfunct.js
+++ 03_basics/04_Arrowfunct.js
@@ -8,3 +8,31 @@ const user = {
     }
 }
 user.welcomeMessage()
+
+// Arrow function
+
+const one = () => {                                 // we can not use this inside a function
+    let username = "NIkhil"
+    console.log(this.username);
+}
+
+one();
+
+
+// Explicit return
+
+const addtwo = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(addtwo(26 , 43));
+
+// Implicit return 
+
+const addthree = (nan1, nan2, nan3) =>  (nan1 + nan2 + nan3)
+
+console.log(addthree(26 , 43, 31));
+
+const sarname = () => ({name : "SAINI"})
+
+console.log(sarname);
