33b722dff31a25964eb1e0f9a62bddd37c4ca79e
Nikhilsaini05 <nikhilsaini5001@gmail.com>
2024-09-03 12:50:53 +0000

    switch ststment

---
+++ 04_interMediate/02_switch.js
@@ -0,0 +1,47 @@
+// Find month 
+
+const month = 9
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("fabuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("september");
+        break;
+    case 10:
+        console.log("october");
+        break;
+        case 11:
+        console.log("november");
+        break;
+    case 12:
+        console.log("december");
+        break;
+
+    default:
+            console.log("not matched");
+            
+        break;
+}
